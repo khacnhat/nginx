d0ee842a7ff3492a13c5bdf03570fdbd8668c972
Jon Jagger <jon@jaggersoft.com>
2016-11-11 11:22:50 +0000

    use docker-compose.yml file to build image

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+hash docker 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker is not installed"
+  exit 1
+fi
+
+my_dir="$( cd "$( dirname "${0}" )" && pwd )"
+
+docker-compose -f ${my_dir}/docker-compose.yml build
