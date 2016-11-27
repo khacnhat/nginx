3203c25f800cf7d4eab9c4ea704f29000fc93604
Jon Jagger <jon@jaggersoft.com>
2016-11-27 07:48:05 +0000

    use longer -- options in scripts

--- build.sh
+++ build.sh
@@ -8,6 +8,13 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
+hash docker-compose 2> /dev/null
+if [ $? != 0 ]; then
+  echo
+  echo "docker-compose is not installed"
+  exit 1
+fi
+
 my_dir="$( cd "$( dirname "${0}" )" && pwd )"
 
-docker-compose -f ${my_dir}/docker-compose.yml build
+docker-compose --file ${my_dir}/docker-compose.yml build
